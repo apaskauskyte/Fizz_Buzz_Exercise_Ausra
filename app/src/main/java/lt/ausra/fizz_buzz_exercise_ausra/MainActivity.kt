4960594d25b4276050fac9f8462da0db4ed7573c
apaskauskyte <a.paskauskyte@gmail.com>
2023-04-10 16:24:33 +0300

    Add function doFizzBuzzBonusTask()

--- app/src/main/java/lt/ausra/fizz_buzz_exercise_ausra/MainActivity.kt
+++ app/src/main/java/lt/ausra/fizz_buzz_exercise_ausra/MainActivity.kt
@@ -2,18 +2,23 @@ package lt.ausra.fizz_buzz_exercise_ausra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var textListView: ListView
     lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        doFizzBuzzTask()
+//        doFizzBuzzTask()
+        doFizzBuzzBonusTask()
     }
 
     private fun doFizzBuzzTask() {
@@ -28,6 +33,32 @@ class MainActivity : AppCompatActivity() {
             } else if (item % 5 == 0) {
                 array[item - 1] = "BUZZ"
             } else { array[item - 1] = item.toString()
+            }
+        }
+
+        textListView = findViewById(R.id.textListView)
+        adapter = ArrayAdapter(
+            this, android.R.layout.simple_expandable_list_item_1, array
+        )
+        textListView.adapter = adapter
+    }
+
+    private fun doFizzBuzzBonusTask() {
+
+        val cycleStart = (-50 until 50).random()
+        val cycleLength = (50 until 150).random()
+        val cycleEnd = cycleLength - abs(cycleStart)
+
+        val array = ArrayList<String>()
+
+        for (item in cycleStart..cycleEnd) {
+            if (item % 3 == 0 && item % 5 == 0) {
+                array.add("FIZZBUZZ")
+            } else if (item % 3 == 0) {
+                array.add("FIZZ")
+            } else if (item % 5 == 0) {
+                array.add("BUZZ")
+            } else { array.add(item.toString())
             }
         }
 
