e2a2164f4c6cd52bdecdb4784908bf1bb0327b5a
apaskauskyte <a.paskauskyte@gmail.com>
2023-04-10 13:54:41 +0300

    Initial commit

---
+++ app/src/main/java/lt/ausra/fizz_buzz_exercise_ausra/MainActivity.kt
@@ -0,0 +1,11 @@
+package lt.ausra.fizz_buzz_exercise_ausra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
